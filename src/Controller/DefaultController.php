168a60942b7688a0ab9ae08cdaf851df52161f3a
jO mAY <104192198+Jomay23@users.noreply.github.com>
2022-07-04 14:13:05 +0200

    Initial Commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Employe;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+
+
+    /**
+     * @Route("/", name="default_home")
+     */
+    public function home(EntityManagerInterface $entityManager): Response
+    {
+        $employes = $entityManager->getRepository(Employe::class)->findAll();
+
+        return $this->render("default/home.html.twig", [
+            # on passe le tableau (array) $employes au pluriel a Twig.
+            "employes" => $employes
+        ]);
+    }
+    
+
+}
